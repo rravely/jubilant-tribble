08b8358cc73f2cc3b6ac083a68b57a5e1f954414
Rahyun <rravely@naver.com>
2023-01-26 23:14:09 +0900

    23.01.26

---
+++ Q_Algorithm/A38.py
@@ -0,0 +1,36 @@
+# 정확한 순위
+# 현대모비스 5번 문제랑 똑같음
+# 플로이드 워셜 알고리즘 수행하고 모든 노드에 대하여 다른 노드와 서로 도달이 가능한지를 체크
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+# 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+result = 0
+
+for i in range(1, n + 1):
+    count = 0
+    for j in range(1, n + 1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1        
+        if count == n:
+            result += 1
+
+print(result)
+
