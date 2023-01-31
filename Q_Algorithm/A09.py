ef0d7ade4297ec39a41c8f9a39ad895d9ec8c233
Rahyun <rravely@naver.com>
2023-01-31 21:22:08 +0900

    23.01.31

--- Q_Algorithm/A09.py
+++ Q_Algorithm/A09.py
@@ -3,7 +3,7 @@
 def solution(s):
     answer = len(s)
     
-    for step in range(1, len(s) // 2 + 1):
+    for step in range(1, len(s) // 2 + 1): 
         compressed = ""
         prev = s[0:step]
         count = 1
